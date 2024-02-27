7ac4b08c5d51cbe9345c563fbedf0f4db16b3d00
davidlboyd1986 <davidlboyd1986@gmail.com>
2024-02-26 20:59:38 -0500

    finished more of ch1

---
+++ src/ch1/exercises/exercise1-12.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define IN  1	/* inside a word */
+#define OUT 0	/* outside a word */
+
+/* print input one word per line */
+main()
+{
+	int character, state;
+	state = OUT;
+
+	while ((character = getchar()) != EOF){
+		if (character == ' ' || character == '\t' || character == '\n'){
+			if (state == IN){
+				putchar('\n');
+				state = OUT;
+			} else {
+				continue;
+			}
+		}else{
+			putchar(character);
+			state = IN;
+		}
+	}
+}
---
+++ src/ch1/exercises/exercise1-13.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define INWORD	 1	 /* Pointer is inside word */
+#define OUTWORD  0	 /* Pointer is outside word */
+
+/* output the frequency of words by word lenght */
+main()
+{
+	int c, i, length, output;
+	length = 0;
+	int ndigit[20];
+	int pointer = OUTWORD;
+
+	for (i = 1; i < 21; ++i){
+		ndigit[i] = 0;
+	}
+
+	while ((c = getchar()) != EOF){
+		if (c == ' ' || c == '\t' || c == '\n'){
+			if (length > 0){
+				++ndigit[length];
+				length = 0;
+				pointer = OUTWORD;
+			}
+		} else{
+			++length;
+			if (pointer == OUTWORD){
+				pointer = INWORD;
+			}
+		}
+	}
+	for (i = 1; i < 21; ++i){
+		printf("%d:",i);
+		for (output = 0; output < ndigit[i]; ++output){
+			printf("#");
+		}
+		printf("\n");	
+	}
+}
