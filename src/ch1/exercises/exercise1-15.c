30416ceb71eb16918d57f0fb3946aac13a9b39ae
davidlboyd1986 <davidlboyd1986@gmail.com>
2024-02-28 21:03:44 -0500

    added function classes

---
+++ src/ch1/exercises/exercise1-15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* 
+#define LOWER	0
+#define UPPER	300
+#define STEP	30
+*/
+
+int tempConvertor(int lower, int upper, int step);
+
+main()
+{
+	tempConvertor(-20, 400, 20);
+}
+
+/* print Fahrenheit-Celsius table */
+int tempConvertor(int lower, int upper, int step)
+{
+	int fahr;
+	for (fahr = lower; fahr <= upper; fahr = fahr + step)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
+}
