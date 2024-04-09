6f03f7bfe49b065518974f3ace29e5a7c6dae57b
davidlboyd1986 <davidlboyd1986@gmail.com>
2024-04-08 21:16:13 -0400

    Exercise1-22 - got it working on a basic level. Need to figure out what to do with spaces for new lines

--- src/ch1/exercises/exercise1-22.c
+++ src/ch1/exercises/exercise1-22.c
@@ -9,16 +9,33 @@ main(){
 	/* Write line in a buffer, so if you have to backtrack to get to previous blank at line input,
 	 * you can read from the buffer. */	
 
-	int c, i, b, a;
+	int c, i, b, a, l;
 	char output[MAXLINE];
 	char buffer[MAXLINE];
 
-	i = 0;
+	i = l = 0;
+	int previousBlank = 0;
+	int lastNonBlank;
 
 	while ((c = getchar()) != EOF){
+		/* fold long lines into two or more lines shorter lines after the last non-blank character
+		 * that occurs before the n-th column of input. For really long lines force break the line */
+		if (previousBlank == 0 && (c == ' ' || c == '\t')){
+			lastNonBlank = i - 1;
+			previousBlank = 1;
+		} else {
+			lastNonBlank = i;
+			previousBlank = 0;
+		}	
+		if (l == LINELENGTH){
+			output[lastNonBlank] = '\n';
+			++i;
+			l = 0;
+		}
 		output[i] = c;
 		buffer[i] = c;
 		++i;
+		++l;
 	}
 
 	/* This won't get ran until you stop input wiht ctrl+d */
