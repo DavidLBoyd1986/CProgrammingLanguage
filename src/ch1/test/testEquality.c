9be2ba9384dc9cb66043f90bb51abdacfa9acaf2
davidlboyd1986 <davidlboyd1986@gmail.com>
2024-05-09 21:09:26 -0400

    test equality and exercise1-24 should work but is not.

--- src/ch1/test/testEquality.c
+++ src/ch1/test/testEquality.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(){
 
 	if ('a' == 'a'){
-		printf("true works for char equality");
+		printf(" a = a ; true works for char equality\n");
 	}
 
+	printf("%d\n", 'a');
 
+	if ('a' == 97){
+		printf(" a = 97 ; true works for int to str comparison\n");
+	}
+
+	char str[100];
+
+	str[0] = 'a';
+
+	printf("%d\n", str[0]);
+
+
+	if (str[0] == 'a'){
+		printf("str[0] == 'a' ; true works for char array item to str comparison\n");
+	}
 	return 0;
 
 }
