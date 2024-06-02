d4ff2d23e5fe611c408350f10b13012719e4ae65
davidlboyd1986 <davidlboyd1986@gmail.com>
2024-06-01 23:30:28 -0400

    Ch2 - Type Limits: Having exercise print out the values from the limits.h file

--- src/ch2/exercises/exercise1-1PrintHeaderMaxValues.c
+++ src/ch2/exercises/exercise1-1PrintHeaderMaxValues.c
@@ -3,93 +3,25 @@
 #include <limits.h>
 #include <float.h>
 
-
-int test_int_max = 2147483645;
-int test_int_min = -2147483645;
-
-// Can also be written "short test_short_max = ########"
-short int test_short_int_max = 32765;
-short test_short_int_min = -32765;
-
-// Can also be written "long test_long_max = ########"
-long int test_long_int_max = 2147483645;
-long test_long_int_min = -2147483645;
-
-// Can also be written "signed test_signed_int_max = ########"
-signed int test_signed_int_max = 2147483645;
-signed int test_signed_int_min = -2147483645;
-
-// Can also be written "long test_unsigned_int_max = ########"
-unsigned int test_unsigned_int_max = 2147483645;
-unsigned int test_unsigned_int_min = -2147483645;
-
 void main(){
 
-	// Prints the min and max for int
-	printf("\nStops at 'int' max\n");	
-	while (test_int_max >= 0){
-		printf("%d\n", test_int_max);
-		test_int_max++;
-	}
+	printf("Signed Char Max is: %d\n", SCHAR_MAX);
+	printf("Signed Char Min is: %d\n", SCHAR_MIN);
+
+	printf("Unsigned Char Max is: %d\n", UCHAR_MAX);
+	printf("USigned Char Min is: %d\n", 0); //UCHAR_MIN is not set in the linits.h file, but it says it is 0
+
+	printf("Char Max is: %d\n", CHAR_MAX);
+	printf("Char Min is: %d\n", CHAR_MIN);
+
+	printf("Signed Short int Max: %d\n", SHRT_MAX);
+	printf("Signed Short int Min: %d\n", SHRT_MIN);
 	
-	printf("\nStops at 'int' min\n");	
-	while (test_int_min <= 0){
-		printf("%d\n", test_int_min);
-		test_int_min--;
-	}
-	
-	printf("\nStops at 'short int' max\n");	
-	while (test_short_int_max >= 0){
-		printf("%d\n", test_short_int_max);
-		test_short_int_max++;
-	}
-	
-	printf("\nStops at 'short int' max\n");	
-	while (test_short_int_min <= 0){
-		printf("%d\n", test_short_int_min);
-		test_short_int_min--;
-	}
-	
-	int n = 0;
-	printf("\nLoops around after 'long int' max to 'long int' min\n");	
-	while (n < 8){
-		printf("%d\n", test_long_int_max);
-		test_long_int_max++;
-		n++;
-	}
+	printf("Unsigned Short int Max: %d\n", USHRT_MAX);
+	printf("Unsigned Short int Min: %d\n", 0); //USHRT_MIN is not defined in the limits.h file, but it says it is 0
+	printf("Signed int Max: %d\n", INT_MAX);
+	printf("Signed int Min: %d\n", INT_MIN);
 
-	n = 0;
-	printf("\nLoops around after 'long int' min to 'log int' min\n");	
-	while (n < 8){
-		printf("%d\n", test_long_int_min);
-		test_long_int_min--;
-		n++;
-	}
-
-	// signed and unsigned int
-	n = 0;
-	printf("\nStops at 'signed int' min\n");	
-	while (n < 8){
-		printf("%d\n", test_signed_int_min);
-		test_signed_int_min--;
-		n++;
-	}
-
-	n = 0;
-	printf("\nStops at 'unsigned int' max\n");	
-	while (n < 8){
-		printf("%d\n", test_unsigned_int_max);
-		test_unsigned_int_max++;
-		n++;
-	}
-
-	n = 0;
-	printf("\nStopa at 'char' max\n");
-	while (n < 300){
-		char c = n;
-		printf("%i = %c, ", n, c);
-		n++;
-	}
-	printf("\nChar seems to be 256 bits it loops back after that.\n");
-
+	printf("Unsigned int Max: %d\n", UINT_MAX) //This isn't working;
+	printf("UnSigned int Min: %d\n", 0); //UINT_MIN is not defined but in the limits.h file, but it says that it is 0
 }
