3b13b98f468b051c3a34b7a75e1d2819feec1248
davidlboyd1986 <davidlboyd1986@gmail.com>
2024-06-10 21:20:41 -0400

    ch2 - type min and max: updated exercise1-1.c to include printing of int and char min and max values stored in the limits.h file.

--- src/ch2/exercises/exercise1-1.c
+++ src/ch2/exercises/exercise1-1.c
@@ -25,6 +25,10 @@ unsigned int test_unsigned_int_min = -2147483645;
 
 void main(){
 
+// Printing values for Max and Min of char types
+
+///-------------------------------------------------------------------
+
 	// Prints the min and max for int
 	printf("\nStops at 'int' max\n");	
 	while (test_int_max >= 0){
@@ -90,6 +94,39 @@ void main(){
 		printf("%i = %c, ", n, c);
 		n++;
 	}
+
+//--------------------------------------------------------------------------
+
+// Print Max and Min Type variables
+	
 	printf("\nChar seems to be 256 bits it loops back after that.\n");
 
+        printf("\n\n");
+        printf("Signed Char Max is: %d\n", SCHAR_MAX);
+        printf("Signed Char Min is: %d\n\n", SCHAR_MIN);
+
+        printf("Unsigned Char Max is: %u\n", UCHAR_MAX);
+        printf("USigned Char Min is: %u\n\n", 0); //UCHAR_MIN is not set in the linits.h file, but it says it is 0
+        printf("Signed Short int Max: %d\n", SHRT_MAX);
+        printf("Signed Short int Min: %d\n\n", SHRT_MIN);
+        
+        printf("Unsigned Short int Max: %u\n", USHRT_MAX);
+        printf("Unsigned Short int Min: %u\n\n", 0); //USHRT_MIN is not defined in the limits.h file, but it says it is 0
+        printf("Signed int Max: %d\n", INT_MAX);
+        printf("Signed int Min: %d\n\n", INT_MIN);
+
+        printf("Unsigned int Max: %u\n", UINT_MAX);
+        printf("UnSigned int Min: %u\n\n", 0); //UINT_MIN is not defined but in the limits.h file, but it says that it is 0
+        printf("Signed long int Max: %ld\n", LONG_MAX);
+        printf("Signed long int Min: %ld\n\n", LONG_MIN);
+        
+        printf("Unsigned long int Max: %lu\n", ULONG_MAX);
+        printf("Unsigned long int Min: %lu\n\n", 0); //ULONG_MIN is not defined but in the limits.h file, but it says that it is 0
+        printf("Signed long long int Max: %lli\n", LLONG_MAX);
+        printf("Signed long long int Min: %lld\n\n", LLONG_MIN);
+
+        printf("Unsigned long long int Max: %llu\n", ULLONG_MAX);
+        printf("Unsigned long long int Min: %d\n\n", 0); //ULLONG_MIN is not defined but in the limits.h file, but it says that it is 0
+
+///-------------------------------------------------------------------
 }
