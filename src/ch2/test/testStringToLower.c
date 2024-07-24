017ca361414dfb79a9c8f66fcd4a61c0c2dfd40a
davidlboyd1986 <davidlboyd1986@gmail.com>
2024-07-23 21:33:46 -0400

    testing - string conversion: testing converting string tolower

---
+++ src/ch2/test/testStringToLower.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int lower(int c);
+
+int main() {
+
+	char string[40] = "HELLO WORLD";
+	char stringOutput[40];
+	int i = 0;
+	int c;
+	while ((c = string[i]) != '\0') {
+		c = lower(c);
+		stringOutput[i] = c;
+		i++;		
+	}
+	printf("\n%s\n", stringOutput);
+}
+
+int lower(int c) {
+
+	if (c >= 'A' && c <= 'Z')
+		return c + 'a' - 'A';
+	else
+		return c;
+}
+
