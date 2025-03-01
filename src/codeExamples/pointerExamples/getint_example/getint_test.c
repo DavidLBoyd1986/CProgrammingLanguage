6b22c34562784e5ab7addf9e4bff53b845158dce
davidlboyd1986 <davidlboyd1986@gmail.com>
2025-03-01 17:01:21 -0500

    Pointers - Messing with pointers

--- src/codeExamples/pointerExamples/getint_example/getint_test.c
+++ src/codeExamples/pointerExamples/getint_example/getint_test.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
-#include "getint_pointer.h"
+#include "getint.h"
 
-#define SIZE 100
+#define SIZE 10
 
-int getint (int *pn);
+void print_array(int array[], int len);
 
 int main()
 {
 	int n, array[SIZE], getint(int *);
-
+	printf("\nIn main before loop\n");
 	for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++)
 		;
+	printf("\nIn main after loop\n");
+	int l = sizeof(array) / sizeof(array[0]);
+	print_array(array, l);
 }
 
-
+void print_array(int array[], int len)
+{
+	int i = 0;
+	printf("\n{");
+	while (i < len-1) {
+		printf("%i, ", array[i]);
+		i++;
+	}
+	printf("%i}\n\n", array[i]);
+}
